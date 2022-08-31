0e2aec153eeebf8e3133ba23814fd951ca968feb
revestM <martinezjc94@hotmail.com>
2022-08-31 20:11:53 -0300

    git config --global user.name "John Doe"

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = null ; adfg fasd
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
@@ -16,7 +16,7 @@ const nuevaResta = 10 - null === 5;
 const nuevaMultiplicacion = 10 * null === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === null;g
 
 
 // En los próximos 22 problemas, deberás completar la función.
