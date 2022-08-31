05c50b470b4ed87a4aced3cb937253e850ff5352
Your Name <martinezjc94@hotmail.com>
2022-08-31 20:19:00 -0300

    firts commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = null ; adfg fasd
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = null;firts comiitttt
 
 // Crea una variable booleana:
 const nuevoBool = null;
